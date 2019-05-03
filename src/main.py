250442ea11a44265da907922e6f29785089a6aeb
Hitesh Paul <git@hiteshpaul.com>
2019-05-03 17:20:04 -0400

    Add server code

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from physics.pong import Game
 from motion.track import Tracker
+from server.run import Server
 
 player  = Tracker(color='blue')
 #p2  = Tracker(color='green')
 game = Game()
+s = Server(host="localhost", topic="player1")
 
 while game.running:
     player.track(show=False)
+    s.send(player.y_loc)
     #opponent = read()
     #send(player.y_loc)
     game.update(player.y_loc,opponent)
