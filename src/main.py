36975b4475dd2a67e2a27a1f657efc54ff2e6070
Hitesh Paul <git@hiteshpaul.com>
2019-05-11 11:56:44 -0400

    Add menu code

--- src/main.py
+++ src/main.py
@@ -1,20 +1,14 @@
 from physics.pong import Game
 from motion.track import Tracker
-<<<<<<< HEAD
 from server.run import Server
-=======
 from ui.menu import Menu
->>>>>>> dbcd45aecacdd7d9eb852ead204371d334d70a49
 
 player  = Tracker(color='blue')
 #p2  = Tracker(color='green')
 game = Game()
-<<<<<<< HEAD
 s = Server(handler=game.opponent, host="localhost", sender="player1", receiver="player2")
-=======
 screen = game.get_screen()
 menu = Menu(screen)
->>>>>>> dbcd45aecacdd7d9eb852ead204371d334d70a49
 
 menu.main_menu()
 
@@ -22,8 +16,4 @@ while True:
     player.track(show=False)
     s.send(player.y_loc)
     #send(player.y_loc)
-<<<<<<< HEAD
     game.update(player.y_loc)
-=======
-    game.update(player.y_loc,0.0)
->>>>>>> dbcd45aecacdd7d9eb852ead204371d334d70a49
